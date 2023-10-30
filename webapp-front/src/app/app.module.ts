c3620237dcd9388cc87986741e1e6b1eaf2db6db
Sanidhya Soni <sanidhyasoni655@gmail.com>
2023-10-30 23:35:28 +0530

    Footrer Added

--- webapp-front/src/app/app.module.ts
+++ webapp-front/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
